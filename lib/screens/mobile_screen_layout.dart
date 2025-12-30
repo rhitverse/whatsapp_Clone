778c3d286b02adf058df3cf71b5e192fcff965dc
rhitverse <rohitsainii.work@gmail.com>
2025-12-30 18:50:05 +0530

    Serach Bar

---
+++ lib/screens/mobile_screen_layout.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/colors.dart';
+
+class MobileScreenLayout extends StatelessWidget {
+  const MobileScreenLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: backgroundColor,
+          title: const Text(
+            'WhatsApp',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 25,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(65),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Container(
+                height: 45,
+                decoration: BoxDecoration(
+                  color: searchBarColor,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: const TextField(
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.search, color: Colors.grey),
+                    hintText: 'Ask Meta AI or Search',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    border: InputBorder.none,
+                    contentPadding: EdgeInsets.symmetric(vertical: 11),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
