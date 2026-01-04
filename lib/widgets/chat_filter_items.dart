b4345999a79dde261da1a7b1a6052bc968f4aeca
rhitverse <rohitsainii.work@gmail.com>
2026-01-04 21:35:05 +0530

    yeah List is done

--- lib/widgets/chat_filter_items.dart
+++ lib/widgets/chat_filter_items.dart
@@ -19,44 +19,50 @@ class _ChatFilterItemsState extends State<ChatFilterItems> {
   ];
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 32,
-      child: ListView.separated(
-        scrollDirection: Axis.horizontal,
-        padding: const EdgeInsets.symmetric(horizontal: 12),
-        itemCount: filters.length,
-        separatorBuilder: (_, _) => const SizedBox(width: 8),
-        itemBuilder: (context, index) {
-          final bool isSelected = selectedIndex == index;
+    return Transform.translate(
+      offset: const Offset(0, -4),
+      child: SizedBox(
+        height: 32,
+        child: ListView.separated(
+          scrollDirection: Axis.horizontal,
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          itemCount: filters.length,
+          separatorBuilder: (_, _) => const SizedBox(width: 8),
+          itemBuilder: (context, index) {
+            final bool isSelected = selectedIndex == index;
 
-          return GestureDetector(
-            onTap: () {
-              setState(() {
-                selectedIndex = index;
-              });
-            },
-            child: AnimatedContainer(
-              duration: const Duration(milliseconds: 200),
-              padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 6),
-              decoration: BoxDecoration(
-                color: isSelected
-                    ? const Color(0xFF1F3D2B)
-                    : const Color(0xff23282c),
-                borderRadius: BorderRadius.circular(30),
-              ),
-              child: Text(
-                filters[index],
-                style: TextStyle(
+            return GestureDetector(
+              onTap: () {
+                setState(() {
+                  selectedIndex = index;
+                });
+              },
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 200),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 14,
+                  vertical: 6,
+                ),
+                decoration: BoxDecoration(
                   color: isSelected
-                      ? Color.fromARGB(255, 129, 230, 174)
-                      : Colors.grey,
-                  fontSize: 13,
-                  fontWeight: FontWeight.w500,
+                      ? const Color(0xFF1F3D2B)
+                      : const Color(0xff23282c),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Text(
+                  filters[index],
+                  style: TextStyle(
+                    color: isSelected
+                        ? Color.fromARGB(255, 129, 230, 174)
+                        : Colors.grey,
+                    fontSize: 13,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
