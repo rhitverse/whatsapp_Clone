b1ba749f4fb90f3bb9e5a09c7a19156efb0f7e5d
rhitverse <rohitsainii.work@gmail.com>
2026-01-10 03:21:37 +0530

    remain

--- lib/screens/mobile_chat_screen.dart
+++ lib/screens/mobile_chat_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:whatsapp_clone/colors.dart';
 import 'package:whatsapp_clone/info.dart';
 import 'package:whatsapp_clone/widgets/chat_list.dart';
+import 'package:whatsapp_clone/widgets/mobile_chat_box.dart';
 
 class MobileChatScreen extends StatefulWidget {
   const MobileChatScreen({super.key});
@@ -12,15 +13,6 @@ class MobileChatScreen extends StatefulWidget {
 }
 
 class _MobileChatScreenState extends State<MobileChatScreen> {
-  final TextEditingController _controller = TextEditingController();
-  bool isTyping = false;
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -79,90 +71,9 @@ class _MobileChatScreenState extends State<MobileChatScreen> {
               child: ChatList(),
             ),
           ),
-
           Padding(
             padding: const EdgeInsets.only(bottom: 12),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.054,
-                  width: MediaQuery.of(context).size.width * 0.88,
-                  padding: const EdgeInsets.symmetric(horizontal: 12),
-                  decoration: BoxDecoration(
-                    color: mobileChatBoxColor,
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                  child: Row(
-                    children: [
-                      SizedBox(width: 12),
-                      SvgPicture.asset(
-                        'assets/svg/Sticker.svg',
-                        color: Colors.white,
-                        width: 26,
-                        height: 26,
-                      ),
-                      SizedBox(width: 10),
-                      Expanded(
-                        child: TextField(
-                          controller: _controller,
-                          cursorColor: Colors.green,
-                          style: const TextStyle(
-                            fontSize: 16,
-                            color: Colors.white,
-                          ),
-                          onChanged: (value) {
-                            setState(() {
-                              isTyping = value.trim().isNotEmpty;
-                            });
-                          },
-                          decoration: InputDecoration(
-                            hintText: 'Message',
-                            hintStyle: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 17,
-                            ),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(20),
-                              borderSide: const BorderSide(
-                                width: 0,
-                                style: BorderStyle.none,
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.attach_file_rounded),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.camera_alt_outlined),
-                      ),
-                    ],
-                  ),
-                ),
-                CircleAvatar(
-                  radius: 23,
-                  backgroundColor: const Color(0xFF00C357),
-                  child: IconButton(
-                    icon: Icon(
-                      isTyping ? Icons.send : Icons.mic,
-                      color: Colors.black,
-                      size: 23,
-                    ),
-                    onPressed: () {
-                      if (isTyping) {
-                        print("Send: ${_controller.text}");
-                      } else {
-                        print("Mic pressed");
-                      }
-                    },
-                  ),
-                ),
-              ],
-            ),
+            child: MobileChatBox(),
           ),
         ],
       ),
---
+++ lib/widgets/mobile_chat_box.dart
@@ -0,0 +1,102 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:whatsapp_clone/colors.dart';
+
+class MobileChatBox extends StatefulWidget {
+  const MobileChatBox({super.key});
+
+  @override
+  State<MobileChatBox> createState() => _MobileChatBoxState();
+}
+
+class _MobileChatBoxState extends State<MobileChatBox> {
+  final TextEditingController _controller = TextEditingController();
+  bool isTyping = false;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height * 0.054,
+          width: MediaQuery.of(context).size.width * 0.88,
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          decoration: BoxDecoration(
+            color: mobileChatBoxColor,
+            borderRadius: BorderRadius.circular(30),
+          ),
+          child: Row(
+            children: [
+              SizedBox(width: 12),
+              SvgPicture.asset(
+                'assets/svg/Sticker.svg',
+                color: Colors.white,
+                width: 26,
+                height: 26,
+              ),
+              SizedBox(width: 10),
+              Expanded(
+                child: TextField(
+                  controller: _controller,
+                  cursorColor: Colors.green,
+                  style: const TextStyle(fontSize: 16, color: Colors.white),
+                  onChanged: (value) {
+                    setState(() {
+                      isTyping = value.trim().isNotEmpty;
+                    });
+                  },
+                  decoration: InputDecoration(
+                    hintText: 'Message',
+                    hintStyle: TextStyle(color: Colors.grey, fontSize: 17),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: const BorderSide(
+                        width: 0,
+                        style: BorderStyle.none,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.attach_file_rounded),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.camera_alt_outlined),
+              ),
+            ],
+          ),
+        ),
+        CircleAvatar(
+          radius: 23,
+          backgroundColor: const Color(0xFF00C357),
+          child: IconButton(
+            icon: Icon(
+              isTyping ? Icons.send : Icons.mic,
+              color: Colors.black,
+              size: 23,
+            ),
+            onPressed: () {
+              if (isTyping) {
+                print("Send: ${_controller.text}");
+              } else {
+                print("Mic pressed");
+              }
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
