0fafaad7b2018fba8bf018e31d5c4e5e838f448c
rhitverse <rohitsainii.work@gmail.com>
2026-01-11 02:36:12 +0530

    i guess

---
+++ lib/features/app/const/message_type_const.dart
@@ -0,0 +1,14 @@
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:whatsapp_clone/colors.dart';
+
+void toast(String message) {
+  Fluttertoast.showToast(
+    msg: message,
+    toastLength: Toast.LENGTH_SHORT,
+    gravity: ToastGravity.BOTTOM,
+    timeInSecForIosWeb: 2,
+    backgroundColor: backgroundColor,
+    textColor: whiteColor,
+    fontSize: 16.0,
+  );
+}
