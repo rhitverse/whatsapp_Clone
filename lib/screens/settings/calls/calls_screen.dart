20e46e2b61d59132f8acf1491199c52beb20bdba
rhitverse <rohitsainii.work@gmail.com>
2026-01-19 00:58:22 +0530

    wakata

--- lib/screens/settings/calls/calls_screen.dart
+++ lib/screens/settings/calls/calls_screen.dart
@@ -8,6 +8,7 @@ class CallsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final favUser = info[0];
     return Scaffold(
       backgroundColor: backgroundColor,
       appBar: AppBar(
@@ -34,107 +35,169 @@ class CallsScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: ListView(
-        children: [
-          const SizedBox(height: 8),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: Text(
-              "Favourites",
-              style: TextStyle(fontWeight: FontWeight.w600),
-            ),
-          ),
-          ListTile(
-            leading: const CircleAvatar(
-              backgroundColor: uiColor,
-              child: Icon(Icons.add, color: Colors.white, size: 26),
-            ),
-            title: const Text("Add favourite"),
-            onTap: () {},
-          ),
-          const SizedBox(height: 8),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: Text(
-              "Recent",
-              style: TextStyle(fontWeight: FontWeight.w600),
-            ),
-          ),
-          ListView.builder(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: info.length,
-            itemBuilder: (context, index) {
-              final user = info[index];
-              final bool isMissed = index % 3 == 1;
-              final bool isVideo = index % 3 == 0;
-
-              return ListTile(
-                leading: CircleAvatar(
-                  backgroundImage: NetworkImage(user['profilePic']!),
+      body: Transform.translate(
+        offset: Offset(0, -6),
+        child: ListView(
+          children: [
+            const SizedBox(height: 8),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Text(
+                "Favourites",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                  fontSize: 18,
                 ),
-                title: Text(
-                  user['name']!,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    color: isMissed ? Colors.red : Colors.white,
+              ),
+            ),
+            SizedBox(height: 6),
+            ListTile(
+              leading: const CircleAvatar(
+                backgroundColor: uiColor,
+                child: Icon(Icons.add, color: Colors.white, size: 26),
+              ),
+              title: const Text(
+                "Add favourite",
+                style: TextStyle(color: Colors.white),
+              ),
+              onTap: () {},
+            ),
+            ListTile(
+              contentPadding: const EdgeInsets.only(left: 16, right: 6),
+              leading: CircleAvatar(
+                backgroundImage: NetworkImage(favUser['profilePic']!),
+              ),
+              title: Text(
+                favUser['name']!,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 14.5,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              subtitle: Row(
+                children: [
+                  SvgPicture.asset(
+                    "assets/svg/outgoing.svg",
+                    width: 16,
+                    height: 16,
+                    color: Colors.grey,
                   ),
-                ),
-                subtitle: Row(
-                  children: [
-                    SvgPicture.asset(
-                      isMissed
-                          ? "assets/svg/missed.svg"
-                          : "assets/svg/outgoing.svg",
-                      width: 16,
-                      height: 16,
-                      color: isMissed ? Colors.grey : Colors.grey,
-                    ),
-                    SizedBox(width: 6),
-                    Text(
-                      isMissed ? "Missed" : "Outgoing",
-                      style: TextStyle(color: Colors.grey),
-                    ),
-                  ],
-                ),
-                trailing: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(
-                      user['time']!,
-                      style: const TextStyle(color: Colors.grey),
-                    ),
-                    const SizedBox(width: 12),
-                    SvgPicture.asset(
-                      "assets/svg/info.svg",
-                      color: Colors.white,
-                    ),
-                  ],
-                ),
-                onTap: () {},
-              );
-            },
-          ),
-          const SizedBox(height: 20),
-          Center(
-            child: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(Icons.lock, size: 14, color: Colors.grey),
-                SizedBox(width: 6),
-                Text(
-                  "Your personal calls are",
-                  style: TextStyle(color: Colors.grey, fontSize: 12),
-                ),
-                Text(
-                  "end-to-end encrypted",
-                  style: TextStyle(color: uiColor, fontSize: 12),
-                ),
-              ],
+                  const SizedBox(width: 6),
+                  const Text("Outgoing", style: TextStyle(color: Colors.grey)),
+                ],
+              ),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    favUser['time']!,
+                    style: const TextStyle(color: Colors.grey, fontSize: 13),
+                  ),
+                  const SizedBox(width: 12),
+                  SvgPicture.asset("assets/svg/info.svg", color: Colors.white),
+                ],
+              ),
+              onTap: () {},
             ),
-          ),
-          const SizedBox(height: 30),
-        ],
+            const SizedBox(height: 8),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Text(
+                "Recent",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Transform.translate(
+              offset: Offset(0, -6),
+              child: ListView.builder(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: info.length,
+                itemBuilder: (context, index) {
+                  final user = info[index];
+                  final bool isMissed = index % 3 == 1;
+                  final bool isVideo = index % 3 == 0;
+
+                  return ListTile(
+                    contentPadding: const EdgeInsets.only(left: 16, right: 6),
+                    leading: CircleAvatar(
+                      radius: 25,
+                      backgroundImage: NetworkImage(user['profilePic']!),
+                    ),
+                    title: Text(
+                      user['name']!,
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 14.5,
+                        color: isMissed ? Colors.red : Colors.white,
+                      ),
+                    ),
+                    subtitle: Row(
+                      children: [
+                        SvgPicture.asset(
+                          isMissed
+                              ? "assets/svg/missed.svg"
+                              : "assets/svg/outgoing.svg",
+                          width: 16,
+                          height: 16,
+                          color: isMissed ? Colors.grey : Colors.grey,
+                        ),
+                        SizedBox(width: 6),
+                        Text(
+                          isMissed ? "Missed" : "Outgoing",
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          user['time']!,
+                          style: const TextStyle(
+                            color: Colors.grey,
+                            fontSize: 13,
+                          ),
+                        ),
+                        const SizedBox(width: 12),
+                        SvgPicture.asset(
+                          "assets/svg/info.svg",
+                          color: Colors.white,
+                        ),
+                      ],
+                    ),
+                    onTap: () {},
+                  );
+                },
+              ),
+            ),
+            const SizedBox(height: 20),
+            Center(
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(Icons.lock, size: 14, color: Colors.grey),
+                  SizedBox(width: 6),
+                  Text(
+                    "Your personal calls are",
+                    style: TextStyle(color: Colors.grey, fontSize: 12),
+                  ),
+                  Text(
+                    " end-to-end encrypted",
+                    style: TextStyle(color: uiColor, fontSize: 12),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 30),
+          ],
+        ),
       ),
     );
   }
